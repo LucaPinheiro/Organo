412b2ad293fb80f6d9d879e398746d2d9bbcbe87
LucaPinheiro <lucapgomes11@gmail.com>
2023-12-05 12:31:07 -0300

    ajustando projeto e adicionando um label e inputs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Banner from './components/Banner/Banner';
+import Banner from './components/Banner/index.js';
+import TextField from './components/TextField/index.js';
 
 function App() {
   return (
     <div className="App">
     <Banner />
+    <TextField />
     </div>
   );
 }
