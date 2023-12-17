dfac67c6052ae2a85dbc44971442f8ea7e572159
LucaPinheiro <lucapgomes11@gmail.com>
2023-12-17 00:17:52 -0300

    trabalhando o value e o onChange

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,10 +1,25 @@
 import "./TextField.css";
 
 const TextField = (props) => {
+  const placeHolderModify = `${props.placeholder}...`;
+
+  let value = "";
+
+  const readOn = (event) => {
+    value = event.target.value;
+    console.log(value);
+  };
+
   return (
     <div className="text-field">
       <label htmlFor="">{props.label}</label>
-      <input required={props.required} type="text" placeholder={props.placeholder} />
+      <input
+        value={value}
+        onChange={readOn}
+        required={props.required}
+        type="text"
+        placeholder={placeHolderModify}
+      />
     </div>
   );
 };
